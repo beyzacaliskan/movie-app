aaee61a3603253f8df447a66672654867ef3545b
Beyza Çalışkan <caliskanbeyza@gmail.com>
2022-08-05 05:02:48 +0300

    main.js and gitignore files change

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 window.axios = require('axios').default;
-window.axios.defaults.headers.common["Authorization"] = "Bearer " + process.env.VUE_APP_AUTH_KEY;
+window.axios.defaults.headers.common["Authorization"] = "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGZkN2ViZDRmYzM0OTRmY2ZkMTkwODJkNjVmMWRkZSIsInN1YiI6IjYyZTVhMDg0ZjFiNTcxMDA1OTU2MGU2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q0tgETdK1n15ZFRVM0LVRdCjihiOzaSpNc-bOvbH5As";
 
 Vue.use(VueProgressBar, {
   color: 'rgb(143, 255, 199)',
